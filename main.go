8bde25558fbbd573fed5e30870e500fa8a828302
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2021-07-12 10:16:00 +0100

    Updated knn

--- main.go
+++ main.go
@@ -17,41 +17,60 @@ func main() {
 		os.Exit(0)
 	}
 
+	fmt.Println("******* DATASET ********")
 	fmt.Println(rawData)
 
-	// cls := knn.NewKnnClassifier("euclidean", "linear", 3)
-	// cls := knn.NewKnnClassifier("euclidean", "kdtree", 3)
-	// cls := knn.NewKnnClassifier("manhattan", "linear", 3)
-	cls := knn.NewKnnClassifier("manhattan", "kdtree", 3)
-	// cls := knn.NewKnnClassifier("cosine", "linear", 4)
-	// cls := knn.NewKnnClassifier("cosine", "kdtree", 4)
+	distFunction := []string{"euclidean", "manhattan", "cosine"}
+	algorithm := []string{"linear", "kdtree"}
+	kNeighbour := []int{1, 2, 3, 4, 5, 6, 7}
 
-	// Shuffle the raw dataset
-	shuffledData := base.Shuffle(rawData)
+	for _, dist := range distFunction {
 
-	//Do a training-test split
-	trainData, testData := base.InstancesTrainTestSplit(shuffledData, 0.60)
-	cls.Fit(trainData)
+		for _, algo := range algorithm {
 
-	//Calculates the Euclidean distance and returns the most popular label
-	trainingPredictions, err := cls.Predict(trainData)
+			for _, k := range kNeighbour {
 
-	testPredictions, err := cls.Predict(testData)
-	if err != nil {
-		panic(err)
+				fmt.Printf("\n\n\n{ \n\tDistribution Function: %s,\n\tAlgorithm: %s, \n\tkNeighbour: %d \n} \n\n\n", dist, algo, k)
+
+				cls := knn.NewKnnClassifier(dist, algo, k)
+
+				// Shuffle the raw dataset
+				shuffledData := base.Shuffle(rawData)
+
+				//Do a training-test split
+				trainData, testData := base.InstancesTrainTestSplit(shuffledData, 0.60)
+				cls.Fit(trainData)
+
+				//Calculates the Euclidean distance and returns the most popular label
+				trainingPredictions, err := cls.Predict(trainData)
+				if err != nil {
+					panic(err)
+				}
+
+				testPredictions, err := cls.Predict(testData)
+				if err != nil {
+					panic(err)
+				}
+
+				// Prints precision/recall metrics
+				trainConfusionMat, err := evaluation.GetConfusionMatrix(trainData, trainingPredictions)
+				if err != nil {
+					panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
+				}
+
+				fmt.Printf("Summary for evaluation of training data\n")
+				fmt.Println(evaluation.GetSummary(trainConfusionMat))
+
+				// Prints precision/recall metrics
+				confusionMat, err := evaluation.GetConfusionMatrix(testData, testPredictions)
+				if err != nil {
+					panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
+				}
+
+				fmt.Printf("Summary for evaluation of training data\n")
+				fmt.Println(evaluation.GetSummary(confusionMat))
+
+			}
+		}
 	}
-
-	// Prints precision/recall metrics
-	trainConfusionMat, err := evaluation.GetConfusionMatrix(trainData, trainingPredictions)
-	if err != nil {
-		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
-	}
-	fmt.Println(evaluation.GetSummary(trainConfusionMat))
-
-	// Prints precision/recall metrics
-	confusionMat, err := evaluation.GetConfusionMatrix(testData, testPredictions)
-	if err != nil {
-		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
-	}
-	fmt.Println(evaluation.GetSummary(confusionMat))
 }
